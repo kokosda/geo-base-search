9de964905ae8808d9947a5e21e4ca046eda99c1e
Victor Ponamarev <vponamarev@guess.com>
2022-03-10 09:07:56 +0300

    Core and Application projects have obtained necessary auxiliary classes. (2)

--- src/GeoBaseSearch.Application/Handlers/GenericQueryHandlerBase.cs
+++ src/GeoBaseSearch.Application/Handlers/GenericQueryHandlerBase.cs
@@ -1,17 +1,18 @@
-﻿namespace GeoBaseSearch.Application.Handlers;
+﻿using GeoBaseSearch.Core.Handlers;
+using GeoBaseSearch.Core.Interfaces;
 
-public class GenericQueryHandlerBase
+namespace GeoBaseSearch.Application.Handlers
 {
-    public abstract class GenericQueryHandlerBase<TQuery, TResult> : IGenericQueryHandler<TQuery, TResult>
-    {
-        public async Task<IResponseContainerWithValue<TResult>> HandleAsync(TQuery query)
-        {
-            if (query == null)
-                throw new ArgumentNullException(nameof(query));
+	public abstract class GenericQueryHandlerBase<TQuery, TResult> : IGenericQueryHandler<TQuery, TResult>
+	{
+		public async Task<IResponseContainerWithValue<TResult>> HandleAsync(TQuery query)
+		{
+			if (query == null)
+				throw new ArgumentNullException(nameof(query));
 
-            return await GetResultAsync(query);
-        }
+			return await GetResultAsync(query);
+		}
 
-        protected abstract Task<IResponseContainerWithValue<TResult>> GetResultAsync(TQuery query);
-    }
+		protected abstract Task<IResponseContainerWithValue<TResult>> GetResultAsync(TQuery query);
+	}
 }
