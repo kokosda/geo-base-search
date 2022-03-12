a5c1729d5e9f3d63b333487bb60731f7ffd2f180
Victor Ponamarev <vponamarev@guess.com>
2022-03-12 10:24:14 +0300

    DependencyRegistrar: infrastructure level dependencies added.

---
+++ src/GeoBaseSearch.Infrastructure/DependencyInjection/DependencyRegistrar.cs
@@ -0,0 +1,18 @@
+﻿using GeoBaseSearch.Infrastructure.DataAccess;
+using GeoBaseSearch.Infrastructure.DataAccess.Abstract;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GeoBaseSearch.Infrastructure.DependencyInjection
+{
+	public static class DependencyRegistrar
+	{
+		public static IServiceCollection AddInfrastructureLevelServices(this IServiceCollection serviceCollection)
+		{
+			serviceCollection.AddSingleton<IInMemoryDatabase, InMemoryDatabase>();
+			serviceCollection.AddSingleton<IGeoBaseLoader, GeoBaseFileLoader>();
+			serviceCollection.AddSingleton<IGeoBaseImageParser, GeoBaseImageParser>();
+
+			return serviceCollection;
+		}
+	}
+}
