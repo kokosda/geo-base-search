b53c77a8adc5aafb26f199b8514b4eb4197dcdbe
Victor Ponamarev <vponamarev@guess.com>
2022-03-13 12:00:58 +0300

    IpController: returning ContentResult instead of NotFound because HttpStatus codes might be different depending on an error returned.

--- src/GeoBaseSearch.Api/Controllers/IpController.cs
+++ src/GeoBaseSearch.Api/Controllers/IpController.cs
@@ -1,4 +1,5 @@
-﻿using GeoBaseSearch.Application.Locations;
+﻿using System.Net;
+using GeoBaseSearch.Application.IpAddresses;
 using GeoBaseSearch.Core.Handlers;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,7 +22,11 @@ public sealed class IpController : ControllerBase
 		var responseContainer = await _locationByIpQueryHandler.HandleAsync(query);
 
 		if (!responseContainer.IsSuccess)
-			return NotFound(responseContainer.Messages);
+			return new ContentResult
+			{
+				Content = responseContainer.Messages,
+				StatusCode = (int)(responseContainer.Value?.HttpStatusCode ?? HttpStatusCode.BadRequest)
+			};
 
 		return new JsonResult(responseContainer.Value);
 	}
