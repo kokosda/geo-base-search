4ca32409dd22d1b413fa3e305891e1c0d223d8ae
Victor Ponamarev <vponamarev@guess.com>
2022-03-13 11:47:10 +0300

    LocationModelExtensions: LocationModel to Location (Domain).

---
+++ src/GeoBaseSearch.Infrastructure/Locations/LocationModelExtensions.cs
@@ -0,0 +1,24 @@
+﻿using GeoBaseSearch.Domain.Locations;
+using GeoBaseSearch.Infrastructure.Models;
+
+namespace GeoBaseSearch.Infrastructure.Locations;
+
+public static class LocationModelExtensions
+{
+	public static Location ToLocation(this LocationModel locationModel)
+	{
+		var result = new Location
+		{
+			City = locationModel.City,
+			Country = locationModel.Country,
+			Id = locationModel.Id,
+			Latitude = locationModel.Latitude,
+			Longitude = locationModel.Longitude,
+			Organization = locationModel.Organization,
+			Postal = locationModel.Postal,
+			Region = locationModel.Region
+		};
+
+		return result;
+	}
+}
