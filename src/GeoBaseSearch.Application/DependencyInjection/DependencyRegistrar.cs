3e862cbb599efc12ed48fbe19716973e2d8ee573
Victor Ponamarev <vponamarev@guess.com>
2022-03-13 15:14:08 +0300

    DependencyRegistrar: query handlers, IpAddressConverter, LocationInMemoryRepository registered.

--- src/GeoBaseSearch.Application/DependencyInjection/DependencyRegistrar.cs
+++ src/GeoBaseSearch.Application/DependencyInjection/DependencyRegistrar.cs
@@ -9,8 +9,8 @@ namespace GeoBaseSearch.Application.DependencyInjection
 	{
 		public static IServiceCollection AddApplicationLevelServices(this IServiceCollection serviceCollection)
 		{
-			serviceCollection.AddScoped<IGenericQueryHandler<LocationsByCityQuery, LocationsByCityDto>>();
-			serviceCollection.AddScoped<IGenericQueryHandler<LocationByIpQuery, LocationByIpDto>>();
+			serviceCollection.AddScoped<IGenericQueryHandler<LocationsByCityQuery, LocationsByCityDto>, LocationsByCitiesQueryHandler>();
+			serviceCollection.AddScoped<IGenericQueryHandler<LocationByIpQuery, LocationByIpDto>, LocationByIpQueryHandler>();
 			return serviceCollection;
 		}
 	}
--- src/GeoBaseSearch.Infrastructure/DependencyInjection/DependencyRegistrar.cs
+++ src/GeoBaseSearch.Infrastructure/DependencyInjection/DependencyRegistrar.cs
@@ -1,5 +1,9 @@
-﻿using GeoBaseSearch.Infrastructure.DataAccess;
+﻿using GeoBaseSearch.Domain.Locations;
+using GeoBaseSearch.Infrastructure.DataAccess;
 using GeoBaseSearch.Infrastructure.DataAccess.Abstract;
+using GeoBaseSearch.Infrastructure.IpAddresses;
+using GeoBaseSearch.Infrastructure.IpAddresses.Interfaces;
+using GeoBaseSearch.Infrastructure.Locations;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace GeoBaseSearch.Infrastructure.DependencyInjection
@@ -11,6 +15,8 @@ namespace GeoBaseSearch.Infrastructure.DependencyInjection
 			serviceCollection.AddSingleton<IInMemoryDatabase, InMemoryDatabase>();
 			serviceCollection.AddSingleton<IGeoBaseLoader, GeoBaseFileLoader>();
 			serviceCollection.AddSingleton<IGeoBaseImageParser, GeoBaseImageParser>();
+			serviceCollection.AddSingleton<IIpAddressConverter, IpAddressConverter>();
+			serviceCollection.AddSingleton<ILocationRepository, LocationInMemoryRepository>();
 
 			return serviceCollection;
 		}
