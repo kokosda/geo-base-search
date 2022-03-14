5d3ed722e5daabe0e11c6b1bac162d376c8d8e2e
Victor Ponamarev <vponamarev@guess.com>
2022-03-14 08:36:35 +0300

    LocationByIpQuery: Ip property made required.

--- src/GeoBaseSearch.Application/IpAddresses/LocationByIpQuery.cs
+++ src/GeoBaseSearch.Application/IpAddresses/LocationByIpQuery.cs
@@ -1,6 +1,9 @@
-﻿namespace GeoBaseSearch.Application.IpAddresses;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GeoBaseSearch.Application.IpAddresses;
 
 public sealed class LocationByIpQuery
 {
+	[Required]
 	public string Ip { get; init; } = string.Empty;
 }
