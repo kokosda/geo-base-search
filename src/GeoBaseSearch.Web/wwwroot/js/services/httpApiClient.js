4134616822bde14e9948f0c1ae120a1acebe7ea4
Victor Ponamarev <vponamarev@guess.com>
2022-03-16 12:59:11 +0300

    HttpClient -> HttpApiClient.

---
+++ src/GeoBaseSearch.Web/wwwroot/js/services/httpApiClient.js
@@ -0,0 +1,19 @@
+﻿class HttpApiClient {
+	get(url) {
+		if (!url)
+			throw new Error("URL is not defined.")
+
+		const result = fetch(url)
+			.then(async response => {
+				let responseResult = await response.text();
+
+				if (!responseResult) {
+					responseResult = `Response status: ${response.status}`;
+					console.error(response);
+				}
+
+				return responseResult;
+			});
+		return result;
+	}
+}
--- src/GeoBaseSearch.Web/wwwroot/js/services/httpClient.js
+++
@@ -1,8 +0,0 @@
-﻿class HttpClient {
-	get(url) {
-		if (!url)
-			throw new Error("URL is not defined.")
-
-		return fetch(url).then(data => data.json()).then(res => res);
-	}
-}
